b2ad67ad162f8e3c87a0ee7a659ac30c962c6250
codewithprofessor <92303718+codewithprofessor@users.noreply.github.com>
2022-01-06 14:07:37 +0500

    Add files via upload

---
+++ 05jan2022/03adj_of_2x2_professor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[2][2];
+    cout<<"Enter the value of A\n";
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin>>a[i][j];
+    
+    cout<<"The Value of A \n";
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+            cout<<a[i][j]<<"  ";
+            cout<<endl;
+    }
+    // adj of 
+    int adj=0;
+    adj=(a[0][0] * a[1][1])-(a[0][1] * a[1][0]);
+    cout<<"the adj of A is = "<<a[0][0] * a[1][1]<<" - "<<a[0][1] * a[1][0]<<" = "<<adj;
+    return 0;
+}
